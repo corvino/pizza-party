4c9ab3b9893adaa65a1ab203c0a9aa2e0abdd020
Nathan Corvino <nathan@corvino.com>
2022-01-26 18:21:24 -0800

    Sort sepcialty labels on top.

--- orders-for-labels.js
+++ orders-for-labels.js
@@ -6,10 +6,26 @@ const Papa = require("papaparse");
 const pizza = require("./lib/pizza");
 const { labelClasses } = require("./data/config.js");
 
+const isSpecial = (slices) => {
+  const specialNames = ["Gluten Free", "Dairy Free Pepperoni", "Vegan"]
+
+  for (const name of specialNames) {
+    if (slices.includes(name)) {
+      return true
+    }
+  }
+  return false
+}
+
 (async () => {
   const orders = await pizza.parseCSVWithHeader("data/orders-collapsed.csv");
 
-  const ordersForLabels = orders.data.filter(o => labelClasses.includes(o.Class));
+  const subOrders = orders.data.filter(o => labelClasses.includes(o.Class));
+
+  const regularOrders = subOrders.filter(o => !isSpecial(o.Orders))
+  const sepcialOrders = subOrders.filter(o => isSpecial(o.Orders))
+
+  const ordersForLabels = sepcialOrders.concat(regularOrders)
 
   await fs.promises.writeFile("data/orders-labels.csv", Papa.unparse(ordersForLabels));
 })();
